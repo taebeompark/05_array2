40993d0e1625e80e11811cb14719f5b070c8ebb7
taebeompark <ptbyahoo12@gmail.com>
2023-07-26 17:19:12 +0900

    완료

--- src/edu/kh/array2/ex/Array2Run.java
+++ src/edu/kh/array2/ex/Array2Run.java
@@ -4,9 +4,9 @@ public class Array2Run {
 
 	public static void main(String[] args) {
 		Array2Example arr2Ex = new Array2Example();
-		//arr2Ex.ex1();
+		arr2Ex.ex1();
 		//arr2Ex.ex2();
-		arr2Ex.ex3();
+		//arr2Ex.ex3();
 	}
 
 }
